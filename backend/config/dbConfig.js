9e0bef425af0fca23440653ad34416a365208ad9
Hocchik <168454935+Hocchik@users.noreply.github.com>
2025-09-29 19:17:22 -0500

    Update dbConfig.js

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
   database: process.env.DB_NAME || 'mototaxi_app',
-  password: process.env.DB_PASS || 'JCr20057!',
+  password: process.env.DB_PASS || '!',
   port: process.env.PORT_DB || 5432,
 });
 
